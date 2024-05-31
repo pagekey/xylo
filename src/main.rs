13476391a82694b83087b26b0123fa126d3aec74
Stephen Grice <stephengrice@live.com>
2024-05-31 12:31:22 -0400

    Add basic CLI with structopt

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use structopt::StructOpt;
+
+
+#[derive(StructOpt, Debug, PartialEq)]
+#[structopt(name="xylo", about="Self-hosted app creation kit")]
+enum Cli {
+    New {
+        project_name: String,
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::from_args();
+
+    match args {
+        Cli::New { project_name } => {
+            println!("New project: {}", project_name);
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_new_subcommand() {
+        let cli = Cli::from_iter(&["xylo", "new", "myproject"]);
+        assert_eq!(cli, Cli::New { project_name: "myproject".into() });
+    }
 }
