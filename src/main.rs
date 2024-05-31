c7d6ede1713266411e9e6baee16144f39e5da0d0
Stephen Grice <stephengrice@live.com>
2024-05-31 12:49:24 -0400

    Add handler functions for cli

--- src/main.rs
+++ src/main.rs
@@ -9,14 +9,26 @@ enum Cli {
     }
 }
 
-fn main() {
-    let args = Cli::from_args();
-
-    match args {
-        Cli::New { project_name } => {
-            println!("New project: {}", project_name);
+trait ProjectHandler {
+    fn handle(&self, cli: &Cli);
+    fn handle_new(&self, project_name: &String);
+}
+struct DefaultProjectHandler;
+impl ProjectHandler for DefaultProjectHandler {
+    fn handle(&self, cli: &Cli) {
+        match cli {
+            Cli::New { project_name } => self.handle_new(project_name)
         }
     }
+    fn handle_new(&self, project_name: &String) {
+        println!("New project: {:?}", project_name);
+    }
+}
+
+fn main() {
+    let args = Cli::from_args();
+    let handler = DefaultProjectHandler;
+    handler.handle(&args);
 }
 
 #[cfg(test)]
@@ -24,7 +36,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_new_subcommand() {
+    fn test_parse_new() {
         let cli = Cli::from_iter(&["xylo", "new", "myproject"]);
         assert_eq!(cli, Cli::New { project_name: "myproject".into() });
     }
