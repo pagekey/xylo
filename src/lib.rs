f7feb35e4a63f011b9952841a2d1626c14e92775
Stephen Grice <stephengrice@live.com>
2024-05-31 14:43:02 -0400

    Add command_successful function

--- src/lib.rs
+++ src/lib.rs
@@ -2,12 +2,33 @@ pub mod cli;
 
 use std::fs;
 use std::path::Path;
+use std::process::Command;
 
 
 pub fn new_project(name: &String) {
     println!("New project!: {}",name);
     fs::create_dir(name);
 }
+
+fn command_successful(command: &str) -> bool {
+    let mut parts_iter = command.split_whitespace();
+    let command_name: &str = parts_iter.next().expect("Failed to parse command.");
+    let args: Vec<&str> = parts_iter.collect();
+    let result = Command::new(command_name)
+        .args(args)
+        .output();
+    match result {
+        Ok(output) => output.status.success(),
+        Err(error) => false
+    }
+}
+// fn requirements_installed() -> bool {
+//     let output = Command::new("npx")
+//         .arg("--version")
+//         .output()
+//         .expect("Failed to execute command");
+//     output.status.success()
+// }
 
 
 #[cfg(test)]
@@ -32,5 +53,18 @@ mod tests {
 
         env::set_current_dir(&original_dir).expect("Failed to change directory back.");
         temp_dir.close().expect("Failed to delete temporary directory.");
+    }
+
+    #[test]
+    fn test_command_successful_returns_true_for_successful_command() {
+        assert_eq!(command_successful("echo hi"), true);
+    }
+    #[test]
+    fn test_command_successful_returns_false_for_failed_command() {
+        assert_eq!(command_successful("false"), false);
+    }
+    #[test]
+    fn test_command_successful_returns_false_for_command_not_found() {
+        assert_eq!(command_successful("this-is-not-a-valid-command someargs"), false);
     }
 }
