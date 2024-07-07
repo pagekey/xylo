f15a392a98afae6ad994ef0da79c17e2e50642b3
Stephen Grice <stephengrice@live.com>
2024-07-07 15:17:26 -0400

    Add compute_next_version

--- scripts/release.py
+++ scripts/release.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
+"""."""
 import enum
+import re
 import subprocess
 from typing import List
 
@@ -18,7 +20,8 @@ PREFIXES = {
 }
 
 
-def get_git_tags():
+def get_git_tags() -> List[str]:
+    """."""
     try:
         result = subprocess.run(
             ["git", "tag"],
@@ -35,7 +38,8 @@ def get_git_tags():
         return []
 
 
-def get_commit_messages_since(commit_hash):
+def get_commit_messages_since(commit_hash) -> List[str]:
+    """."""
     try:
         result = subprocess.run(
             ["git", "log", f"{commit_hash}..HEAD", "--pretty=format:%s"],
@@ -52,7 +56,8 @@ def get_commit_messages_since(commit_hash):
         return []
 
 
-def compute_release_type(commits: List[str]):
+def compute_release_type(commits: List[str]) -> ReleaseType:
+    """."""
     release_type = ReleaseType.NO_RELEASE
     for commit in commits:
         for prefix, prefix_release_type in PREFIXES.items():
@@ -61,6 +66,35 @@ def compute_release_type(commits: List[str]):
                 if release_type.value < prefix_release_type.value:
                     release_type = prefix_release_type
     return release_type
+
+
+def compute_next_version(release_type: ReleaseType, tags: List[str]) -> str:
+    """."""
+    if len(tags) < 1:
+        return "v0.1.0"
+    pattern = r'^v\d+\.\d+\.\d+$'
+    max_version = (0, 1, 0)
+    for tag in tags:
+        if re.match(pattern, tag):
+            # This tag has valid format (vX.Y.Z)
+            major, minor, patch = tag.replace("v", "").split(".")
+            major = int(major)
+            minor = int(minor)
+            patch = int(patch)
+            if major > max_version[0] \
+                or (major == max_version[0] and minor > max_version[1]) \
+                    or (major == max_version[0] and minor == max_version[1] and patch > max_version[2]):
+                max_version = (major, minor, patch)
+    if release_type == ReleaseType.MAJOR:
+        max_version = (max_version[0] + 1, 0, 0)
+    elif release_type == ReleaseType.MINOR:
+        max_version = (max_version[0], max_version[1] + 1, 0)
+    elif release_type == ReleaseType.PATCH:
+        max_version = (max_version[0], max_version[1], max_version[2] + 1)
+    else:
+        pass # NO_RELEASE
+
+    return f"v{max_version[0]}.{max_version[1]}.{max_version[2]}"
 
 if __name__ == "__main__":
     tags = get_git_tags()
--- scripts/test_release.py
+++ scripts/test_release.py
@@ -5,6 +5,7 @@ from release import (
     get_commit_messages_since,
     ReleaseType,
     compute_release_type,
+    compute_next_version,
 )
 
 
@@ -51,6 +52,7 @@ def test_get_commit_messages_since_with_valid_hash_returns_list_of_messages(mock
     assert result[0] == "Do something"
     assert result[1] == "Do something else"
 
+
 def test_compute_release_type_with_no_prefixes_returns_no_release():
     # Arrange.
     commits = ["nothing important", "another poorly formatted commit message"]
@@ -58,6 +60,7 @@ def test_compute_release_type_with_no_prefixes_returns_no_release():
     result = compute_release_type(commits)
     # Assert.
     assert result == ReleaseType.NO_RELEASE
+
 
 def test_compute_release_type_with_only_fix_returns_patch():
     # Arrange.
@@ -67,18 +70,73 @@ def test_compute_release_type_with_only_fix_returns_patch():
     # Assert.
     assert result == ReleaseType.PATCH
 
+
 def test_compute_release_type_with_fix_and_feat_returns_minor():
     # Arrange.
-    commits = ["fix: Somewhat important", "feat: another poorly formatted commit message"]
+    commits = [
+        "fix: Somewhat important",
+        "feat: another poorly formatted commit message",
+    ]
     # Act.
     result = compute_release_type(commits)
     # Assert.
     assert result == ReleaseType.MINOR
 
+
 def test_compute_release_type_with_major_returns_major():
     # Arrange.
-    commits = ["fix: Do something somewhat important", "feat: Add something", "major: Wow this is a big deal"]
+    commits = [
+        "fix: Do something somewhat important",
+        "feat: Add something",
+        "major: Wow this is a big deal",
+    ]
     # Act.
     result = compute_release_type(commits)
     # Assert.
     assert result == ReleaseType.MAJOR
+
+
+def test_compute_next_version_with_no_existing_tags_returns_default_value():
+    # Arrange.
+    release_type = ReleaseType.MAJOR
+    tags = []
+    # Act.
+    result = compute_next_version(release_type, tags)
+    # Assert.
+    assert result == "v0.1.0"
+
+def test_compute_next_version_with_no_release_returns_greatest_tag():
+    # Arrange.
+    release_type = ReleaseType.NO_RELEASE
+    tags = ["v0.1.0", "v0.3.0", "v0.2.0", "unrelated-tag"]
+    # Act.
+    result = compute_next_version(release_type, tags)
+    # Assert.
+    assert result == "v0.3.0"
+
+def test_compute_next_version_with_patch_bumps_patch_value():
+    # Arrange.
+    release_type = ReleaseType.PATCH
+    tags = ["v0.1.0", "v0.3.2", "v0.2.0", "unrelated-tag"]
+    # Act.
+    result = compute_next_version(release_type, tags)
+    # Assert.
+    assert result == "v0.3.3"
+
+def test_compute_next_version_with_minor_bumps_minor_value():
+    # Arrange.
+    release_type = ReleaseType.MINOR
+    tags = ["v0.1.0", "v0.3.2", "v0.2.0", "unrelated-tag"]
+    # Act.
+    result = compute_next_version(release_type, tags)
+    # Assert.
+    assert result == "v0.4.0"
+
+def test_compute_next_version_with_major_bumps_major_value():
+    # Arrange.
+    release_type = ReleaseType.MAJOR
+    tags = ["v0.1.0", "v0.3.2", "v0.2.0", "unrelated-tag"]
+    # Act.
+    result = compute_next_version(release_type, tags)
+    # Assert.
+    assert result == "v1.0.0"
