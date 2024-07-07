b105ea0dd922c2eabbd457d586955030d848d979
Stephen Grice <stephengrice@live.com>
2024-07-07 11:21:59 -0400

    Add release test script

--- scripts/release.py
+++ scripts/release.py
@@ -1,3 +1,27 @@
 #!/usr/bin/env python3
+import subprocess
 
-print("Hello world")
+
+def get_git_tags():
+    try:
+        result = subprocess.run(
+            ['git', 'tag'],
+            check=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            text=True
+        )
+        # Split the output into lines (each tag is on a new line)
+        tags = result.stdout.strip().split('\n')
+        return tags
+    except subprocess.CalledProcessError as e:
+        print(f"Error getting git tags: {e.stderr}")
+        return []
+
+if __name__ == "__main__":
+    tags = get_git_tags()
+    if tags:
+        for tag in tags:
+            print("The tag:",tag)
+    else:
+        print("No tags")
---
+++ scripts/test_release.py
@@ -0,0 +1,2 @@
+def test_release_does_nothing():
+    pass
