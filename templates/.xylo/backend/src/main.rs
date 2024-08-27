abfc4239ff0668a354b8b19f755dfaf81b57d17b
PageKey Semver <semver@pagekey.io>
2024-08-26 23:12:04 -0400

    fix: Allow any origin with CORS

--- templates/.xylo/backend/src/main.rs
+++ templates/.xylo/backend/src/main.rs
@@ -4,8 +4,12 @@ use warp::Filter;
 #[tokio::main]
 async fn main() {
     println!("Starting backend.");
+    let cors = warp::cors()
+        .allow_any_origin();
+
     let hello = warp::path::end()
-        .map(|| warp::reply::html("hello world"));
+        .map(|| warp::reply::html("hello world"))
+        .with(cors);
 
     // Start the warp server
     warp::serve(hello)
