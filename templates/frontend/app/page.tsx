16bebf99034feb7f50e565ac3ed1bccd77474da2
PageKey Semver <semver@pagekey.io>
2024-08-26 21:59:10 -0400

    feat: Upgrade to Next 14
    Also get Mantine working

---
+++ templates/frontend/app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@mantine/core";
+
+
+export default function Home() {
+  return (
+    <div>
+      Welcome to your new Xylo app.
+      <Button color="red">This is a button</Button>
+    </div>
+  )
+}
