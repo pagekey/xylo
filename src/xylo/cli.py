3993ac3cd07b4e5707583bb03e39d2e3349f0f8a
PageKey Semver <semver@pagekey.io>
2024-09-14 11:32:22 -0400

    Add back nextjs frontend gen

--- src/xylo/cli.py
+++ src/xylo/cli.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 import click
 from cookiecutter.main import cookiecutter
@@ -8,13 +9,28 @@ def xylo():
 
 @xylo.command()
 def new():
-    print("new")
     templates_dir = Path(__file__).parent / "templates"
-    cookiecutter(str(templates_dir))
+    name = input("Name for your app (my-app): ")
+    if len(name) < 1:
+        name = "my-app"
+    cookiecutter(
+        str(templates_dir),
+        extra_context={"name": name},
+        no_input=True,
+    )
+    ignore_file = Path(name) / ".ignore"
+    ignore_file.rename(Path(name) / ".gitignore")
+
+
 
 @xylo.command()
 def dev():
-    print("dev")
+    # check if nextjs app generated yet
+    os.chdir(".xylo/frontend")
+    if not Path("node_modules").exists():
+        os.system("npm i")
+    os.system("npm run dev")
+    os.chdir("../..")
 
 @xylo.command()
 def build():
---
+++ src/xylo/templates/{{cookiecutter.name}}/.xylo/frontend/README.md
@@ -0,0 +1,11 @@
+# Xylo Project
+
+Fill this README in with your description.
+
+## Getting Started
+
+Run the following to develop your app:
+
+```bash
+xylo dev
+```
